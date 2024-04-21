d0a156c54ca88c47077ff0d01ba0ee152aac2b52
nistha-singh <nistha20003@gmail.com>
2024-04-21 21:56:37 +0530

    frontend done

---
+++ ooad/src/main/java/com/bookonrails/ooad/OoadApplication.java
@@ -0,0 +1,15 @@
+package com.bookonrails.ooad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication
+@EnableTransactionManagement
+public class OoadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OoadApplication.class, args);
+	}
+
+}
